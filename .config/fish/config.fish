494decf3705a3502eb90a15a656eb99d696cf403
Marijn de Rijk <derijkmarijn00@gmail.com>
2024-05-29 15:26:54 +0200

    added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g fish_key_bindings fish_vi_key_bindings
+
+# use the .bash_aliases file
+source ~/.bash_aliases
