62c6b95011ab555a4c2449737d7c5b9532853ebc
Marijn de Rijk <derijkmarijn00@gmail.com>
2026-02-13 15:29:44 +0100

    Initial public commit, cleaned history

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,52 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+set -g fish_key_bindings fish_vi_key_bindings
+
+fish_add_path -p ~/.local/bin
+fish_add_path -p ~/bin
+
+# rustup shell setup
+fish_add_path -p $HOME/.cargo/bin
+
+# use zoxide
+zoxide init fish | source
+
+# Starship
+function starship_transient_prompt_func
+    starship module character
+end
+
+function starship_transient_rprompt_func
+    starship module time
+end
+
+starship init fish | source
+enable_transience
+
+# Source helper functions (inspired by omarchy)
+source $__fish_config_dir/helpers.fish
+
+# use the 1password plugin
+if test -e ~/.config/op/plugins.sh
+    source ~/.config/op/plugins.sh
+end
+
+# get venv to work
+set -gx WORKON_HOME $HOME/.virtualenvs
+set -gx VIRTUALENVWRAPPER_PYTHON /usr/bin/python3
+
+# set 1password as the ssh key agent
+if not set -q SSH_AUTH_SOCK
+    set SSH_AUTH_SOCK $HOME/.1password/agent.sock
+end
+
+# aliases
+alias cat='bat'
+alias la='ls -a'
+alias cd='z'
+alias ls='eza --icons'
+alias lg='lazygit'
---
+++ dot_config/private_fish/helpers.fish
@@ -0,0 +1,93 @@
+# Compression
+function compress
+    set -l input (string trim -r -c '/' "$argv[1]")
+    tar -czf "$input.tar.gz" "$input"
+end
+
+alias decompress="tar -xzf"
+
+# Write iso file to sd card
+function iso2sd
+    if test (count $argv) -ne 2
+        echo "Usage: iso2sd <input_file> <output_device>"
+        echo "Example: iso2sd ~/Downloads/ubuntu-25.04-desktop-amd64.iso /dev/sda"
+        echo -e "\nAvailable SD cards:"
+        lsblk -d -o NAME | grep -E '^sd[a-z]' | awk '{print "/dev/"$1}'
+    else
+        sudo dd bs=4M status=progress oflag=sync if="$argv[1]" of="$argv[2]"
+        sudo eject $argv[2]
+    end
+end
+
+# Format an entire drive for a single partition using exFAT
+function format-drive
+    if test (count $argv) -ne 2
+        echo "Usage: format-drive <device> <name>"
+        echo "Example: format-drive /dev/sda 'My Stuff'"
+        echo -e "\nAvailable drives:"
+        lsblk -d -o NAME -n | awk '{print "/dev/"$1}'
+    else
+        echo "WARNING: This will completely erase all data on $argv[1] and label it '$argv[2]'."
+        read -l -p "Are you sure you want to continue? (y/N): " confirm
+
+        if string match -q -r '^[Yy]$' "$confirm"
+            sudo wipefs -a "$argv[1]"
+            sudo dd if=/dev/zero of="$argv[1]" bs=1M count=100 status=progress
+            sudo parted -s "$argv[1]" mklabel gpt
+            sudo parted -s "$argv[1]" mkpart primary 1MiB 100%
+
+            if string match -q '*nvme*' "$argv[1]"
+                set -l partition "$argv[1]p1"
+            else
+                set -l partition "$argv[1]1"
+            end
+
+            sudo partprobe "$argv[1]" || true
+            sudo udevadm settle || true
+
+            sudo mkfs.exfat -n "$argv[2]" "$partition"
+
+            echo "Drive $argv[1] formatted as exFAT and labeled '$argv[2]'."
+        end
+    end
+end
+
+# Transcode a video to a good-balance 1080p that's great for sharing online
+function transcode-video-1080p
+    set -l base (string replace -r '\.[^.]*$' '' "$argv[1]")
+    ffmpeg -i "$argv[1]" -vf scale=1920:1080 -c:v libx264 -preset fast -crf 23 -c:a copy "$base-1080p.mp4"
+end
+
+# Transcode a video to a good-balance 4K that's great for sharing online
+function transcode-video-4K
+    set -l base (string replace -r '\.[^.]*$' '' "$argv[1]")
+    ffmpeg -i "$argv[1]" -c:v libx265 -preset slow -crf 24 -c:a aac -b:a 192k "$base-optimized.mp4"
+end
+
+# Transcode any image to JPG image that's great for shrinking wallpapers
+function img2jpg
+    set -l img "$argv[1]"
+    set -l args $argv[2..]
+    set -l base (string replace -r '\.[^.]*$' '' "$img")
+    magick "$img" $args -quality 95 -strip "$base-optimized.jpg"
+end
+
+# Transcode any image to JPG image that's great for sharing online without being too big
+function img2jpg-small
+    set -l img "$argv[1]"
+    set -l args $argv[2..]
+    set -l base (string replace -r '\.[^.]*$' '' "$img")
+    magick "$img" $args -resize 1080x\> -quality 95 -strip "$base-optimized.jpg"
+end
+
+# Transcode any image to compressed-but-lossless PNG
+function img2png
+    set -l img "$argv[1]"
+    set -l args $argv[2..]
+    set -l base (string replace -r '\.[^.]*$' '' "$img")
+    magick "$img" $args -strip -define png:compression-filter=5 \
+        -define png:compression-level=9 \
+        -define png:compression-strategy=1 \
+        -define png:exclude-chunk=all \
+        "$base-optimized.png"
+end
