744bc77a6072df8398b67b51c8142c316c397347
Marijn de Rijk <derijkmarijn00@gmail.com>
2026-05-19 16:28:33 +0200

    Add .config/hypr/autostart.lua Add .config/hypr/bindings.lua Add .config/hypr/hyprland.lua Add .config/hypr/input.lua Add .config/hypr/looknfeel.lua Add .config/hypr/monitors.lua

---
+++ dot_config/hypr/autostart.lua
@@ -0,0 +1,7 @@
+-- Extra autostart processes.
+-- o.launch_on_start("my-service")
+
+o.launch_on_start("signal-desktop")
+o.launch_on_start("thunderbird")
+o.launch_on_start("zen-nvidia")
+o.launch_webapp("https://web.whatsapp.com/")
---
+++ dot_config/hypr/hyprland.lua
@@ -0,0 +1,30 @@
+-- Learn how to configure Hyprland: https://wiki.hypr.land/Configuring/Start/
+
+-- Load user modules from ~/.config and Omarchy defaults from $OMARCHY_PATH.
+package.path = os.getenv("HOME")
+    .. "/.config/?.lua;"
+    .. (os.getenv("OMARCHY_PATH") or (os.getenv("HOME") .. "/.local/share/omarchy"))
+    .. "/?.lua;"
+    .. package.path
+
+-- All Omarchy default setups
+require("default.hypr.omarchy")
+
+-- Change your own setup in these files and override defaults.
+require("hypr.monitors")
+require("hypr.input")
+require("hypr.bindings")
+require("hypr.looknfeel")
+require("hypr.autostart")
+
+-- Toggle config flags dynamically.
+require("default.hypr.toggles")
+
+-- float planify
+o.window("io.github.alainm23.planify", { float = true })
+
+-- default workspaces
+o.window("org.mozilla.Thunderbird", { workspace = 10 })
+o.window("signal", { workspace = 11 })
+o.window("chrome-web.whatsapp.com__-Default", { workspace = 11 })
+o.window("zen", { workspace = 2 })
