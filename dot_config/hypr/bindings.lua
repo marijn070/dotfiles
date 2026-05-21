d5cbfa79464e08706ff893ad5321e0035344ccbd
Marijn de Rijk <derijkmarijn00@gmail.com>
2026-05-21 15:08:20 +0200

    Update .config/hypr/bindings.lua

--- dot_config/hypr/bindings.lua
+++ dot_config/hypr/bindings.lua
@@ -27,7 +27,7 @@ o.bind("SUPER + SHIFT + I", "Immich", { webapp = "https://photos.marijnderijk.co
 
 o.bind("SUPER + SHIFT + S", "Google Maps", { webapp = "https://maps.google.com/", focus = true })
 
--- Configure
+-- Configure [launch the edit configs script]
 o.bind("SUPER + SHIFT + C", "Config", { launch = "edit-configs" })
 
 -- Maximize, Close
