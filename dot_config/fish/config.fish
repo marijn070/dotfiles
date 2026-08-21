a7587ca52e1fc57fc31f864f6585446a6dbc103c
Marijn de Rijk <derijkmarijn00@gmail.com>
2026-08-22 01:17:04 +0200

    Add .config/fish/config.fish

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,68 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+set -g fish_key_bindings fish_vi_key_bindings
+
+fish_add_path -p ~/.local/bin
+fish_add_path -p ~/bin
+fish_add_path -p ~/.cargo/bin
+
+# rustup shell setup
+fish_add_path -p $HOME/.cargo/bin
+
+# use zoxide
+zoxide init fish | source
+
+# Starship
+
+# Disable any custom config (like nushell)
+set -e STARSHIP_CONFIG
+
+function starship_transient_prompt_func
+    starship module character
+end
+
+function starship_transient_rprompt_func
+    starship module time
+end
+
+starship init fish | source
+enable_transience
+
+# Source helper functions (inspired by omarchy)
+source $__fish_config_dir/helpers.fish
+
+# get venv to work
+set -gx WORKON_HOME $HOME/.virtualenvs
+set -gx VIRTUALENVWRAPPER_PYTHON /usr/bin/python3
+
+#use the 1password plugin
+if test -e ~/.config/op/plugins.sh
+    source ~/.config/op/plugins.sh
+end
+# set 1password as the ssh key agent and
+if not set -q SSH_AUTH_SOCK
+    set SSH_AUTH_SOCK $HOME/.1password/agent.sock
+end
+
+# get op cli completions
+if command -q op
+    op completion fish | source
+end
+
+# Use bat as man pager with nice colors
+batman --export-env | source
+
+# aliases
+alias cat='bat'
+alias la='ls -a'
+alias cd z
+alias ls='eza --icons'
+alias lg='lazygit'
+alias ld='lazydocker'
+alias copy='wl-copy'
+
+direnv hook fish | source
